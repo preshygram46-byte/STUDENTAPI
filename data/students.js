eaffa29b7a40acfa9bad1edb85f3eeab05264c5f
Preshy Gram <preshygram46@gmail.com>
2026-07-25 19:02:24 +0100

    Update students.js

--- data/students.js
+++ data/students.js
@@ -3,8 +3,19 @@ let students = [
         id: 1,
         name: "Precious Ogunlesi",
         age: 20,
-        course: "Computer Science"
-    }
+        course: "Computer"
+        },
+  {
+    id: 2,
+    name: "Femi",
+    age: 22,
+    course: "Software Engineering"
+  },
+  {
+    id: 3,
+    name: "Aisha",
+    age: 19,
+    course: "Information Technology"
+  }
 ];
-
 module.exports = students;
